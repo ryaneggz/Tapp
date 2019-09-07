e45bd5cec512451aea199e08ca1016d55bb8f7ef
unknown <kre8mymedia@gmail.com>
2019-09-07 03:17:34 -0500

    added auth user to profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -23,7 +23,7 @@
 									<div class="overlay"></div>
 									<div class="profile-main">
 										<img src="assets/img/user-medium.png" class="img-circle" alt="Avatar">
-										<h3 class="name">Samuel Gold</h3>
+										<h3 class="name">{{Auth::user()->name}}</h3>
 										<span class="online-status status-available">Available</span>
 									</div>
 									<div class="profile-stat">
