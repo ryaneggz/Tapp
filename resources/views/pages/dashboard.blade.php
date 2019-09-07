5664baf4de694479ade705343eb9c13a0c6537a9
unknown <kre8mymedia@gmail.com>
2019-09-07 16:18:43 -0500

    fixed overflow on dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -95,23 +95,27 @@
 										<button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
 									</div>
 								</div>
-								<div class="pl-2 slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 430px;"><div class="panel-body" style="overflow: hidden; width: auto; height: 430px;">
-									<ul class="list-unstyled activity-list">
-										@if(count($summaries) > 0)
-											@foreach($summaries as $summary)
-												<li>
-													<img src="assets/img/user1.png" alt="Avatar" class="img-circle pull-left avatar">
-												<p><a href="/summaries/{{$summary->id}}">{{$summary->employee->user->name}}</a><br> {!!$summary->body!!} <br><span class="timestamp pull-right">{{$summary->updated_at}}</span></p>
-												</li>
-											@endforeach
-												@else
+								<div class="slimScrollDiv" style="position: relative; width: auto; height: 430px;">
+									<div class="panel-body" style="overflow: auto; width: auto; height: 430px;">
+										<ul class="list-unstyled activity-list">
+											@if(count($summaries) > 0)
+												@foreach($summaries as $summary)
+													<li>
+														<img src="assets/img/user1.png" alt="Avatar" class="img-circle pull-left avatar">
+														<p><a href="/summaries/{{$summary->id}}">{{$summary->employee->user->name}}</a><br> {!!$summary->body!!} <br><span class="timestamp pull-right">{{$summary->updated_at}}</span></p>
+													</li>
+												@endforeach
+											@else
 												<li>
 													<h3>No records found</h3>
 												</li>
 											@endif
-									</ul>
-									<button type="button" class="btn btn-primary btn-bottom center-block">Load More</button>
-								</div><div class="slimScrollBar" style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: 18px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 324.386px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+										</ul>
+										<button type="button" class="btn btn-primary btn-bottom center-block">Load More</button>
+									</div>
+								<div class="slimScrollBar" style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: 18px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 324.386px;"></div>
+								<div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div>
+							</div>
 							</div>
 							<!-- END TIMELINE -->
 						</div>
