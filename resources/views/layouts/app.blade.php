3fca0e60e33a66635ab15a6c8a73e5bd1b9a906c
unknown <kre8mymedia@gmail.com>
2019-10-14 11:47:25 -0500

    added echos back to figure out bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,5 @@
   <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
   <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
 
-  <!-- Load our React component. -->
-  <script src="like_button.js"></script>
 </body>
 </html>
