d3a36569f5fb27f5991cb0ad667af17d1de5353c
unknown <kre8mymedia@gmail.com>
2019-10-25 21:04:33 -0500

    added seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $x = new User;
+        $x->name = 'Mr. Test';
+        $x->employee_id = '1';
+        $x->email = 'test@test.com';
+        $x->password = '$2y$10$Jj6Yiz7uljGHk.retaOaSud9vNR6Ks7udf1WA6u.jjEv4v6hS18ti';
+        $x->save();
+
+        $x = new User;
+        $x->name = 'Mr. Test B';
+        $x->employee_id = '2';
+        $x->email = 'b-test@test.com';
+        $x->password = '$2y$10$Jj6Yiz7uljGHk.retaOaSud9vNR6Ks7udf1WA6u.jjEv4v6hS18ti';
+        $x->save();
     }
 }
