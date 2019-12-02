77739818f6140782ae4761f21b8c400711763410
unknown <kre8mymedia@gmail.com>
2019-12-01 18:17:54 -0600

    few more fixes

--- resources/views/timecards/index.blade.php
+++ resources/views/timecards/index.blade.php
@@ -56,7 +56,7 @@
                   </div>
                   <div class="panel-footer">
                     <div class="row">
-                    <div class="col-xs-6"><span class="panel-note">Last Clock-out: {{$last_timeout->updated_at}}</span></div>
+                    <div class="col-xs-6"><span class="panel-note">Last Clock-out: {{ date('n/d/y | g:i A', $last_timeout->time_out) }}</span></div>
                       <div class="col-md-6 text-right"><a href="#" class="btn btn-primary">View All Shifts</a></div>
                     </div>
                   </div>
