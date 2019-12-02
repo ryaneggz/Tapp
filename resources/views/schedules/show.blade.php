81c4de2e16c491d6477ad7751da57e2596a6874b
unknown <kre8mymedia@gmail.com>
2019-12-01 18:22:08 -0600

    few more fixes

--- resources/views/schedules/show.blade.php
+++ resources/views/schedules/show.blade.php
@@ -65,7 +65,7 @@
                 {{ Form::close() }}
                 <div class="panel-footer">
                   <div class="row">
-                    <div class="col-md-6"><span class="panel-note"><span class="panel-note">Created: {{$schedule->created_at}}</span></div>
+                    <div class="col-xs-6"><span class="panel-note"><span class="panel-note">Created: {{$schedule->created_at}}</span></div>
                     <div class="col-md-6 text-right"><a href="/schedules/{{$schedule->id}}/edit" class="btn btn-primary">Edit Schedule</a></div>
                   </div>
                 </div>
